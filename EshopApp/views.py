39b07da5d264a9a90c1d55ef2a37d49200138752
sumit saxena <sumit.saxena768@gmail.com>
2020-12-19 15:04:26 +0530

    second commit

--- EshopApp/views.py
+++ EshopApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Category
 # Create your views here.
 def index(request):
     product=Product.objects.all()
-    return render(request,'index.html',{'product':product})
+    category=Category.objects.all()
+    return render(request,'index.html',{'product':product,'category':category})
+
+
